1dedbbbff4167c133bd664b7606015b0a456bcc8
residenttrickster <metkusblackus@gmail.com>
2020-07-04 22:48:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+function map(arr, callback) {
+    return arr.map(element => callback(element))
+};
+
+function reduce(arr, callback, startPoint=0) {
+    if(!startPoint) {
+        return arr.reduce((accumulator, element) => callback(accumulator, element))
+    } else {
+        return arr.reduce((accumulator, element, startPoint) => callback(accumulator, element), startPoint)
+    }
+};
+
+
